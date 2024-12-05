6d0531396f39dd14e4f2586fb025ee9f2c0e00d3
RomNox <95658286+RomNox@users.noreply.github.com>
2024-12-06 00:44:09 +0100

    new commit

---
+++ src/test/java/tests/UploadTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import com.herokuapp.pages.UploadPage;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class UploadTest extends TestBase {
+
+    private UploadPage uploadPage;
+
+    @BeforeMethod
+    public void setUpPage() {
+        uploadPage = new UploadPage(driver);
+        uploadPage.navigateToUploadPage();
+    }
+
+    @Test
+    public void testFileUpload() {
+        String filePath = System.getProperty("user.dir") + "/src/test/java/resources/sample.txt";
+
+        uploadPage.uploadFile(filePath);
+
+        String uploadedFileName = uploadPage.getUploadedFileName();
+        assertEquals(uploadedFileName, "sample.txt", "Файл был загружен некорректно");
+    }
+}
